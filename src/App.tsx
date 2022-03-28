8e044e39702b063b2fe2080f2c01643726bead32
ichihatsu1 <ichihatsu1@gmail.com>
2022-03-28 10:05:44 +0700

    Feat : Add Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 export default function App() {
   return <div className="">
-
+    <BrowserRouter >
+      {/* <Sidebar/> */}
+      {/* <Navbar/> */}
+      <Routes>
+        <Route path="/login" element={} />
+        <Route path="/dashboard" element={} />
+        <Route path="/carlist" element={} />
+        <Route path="/carlist/add_new_car" element={} />
+        <Route path='*' element={} />
+      </Routes>
+    </BrowserRouter >
   </div>
 }
