151f2ec748f96c9debdbe427b3be299d7da4cda1
ichihatsu1 <ichihatsu1@gmail.com>
2022-03-29 09:16:25 +0700

    Feat : Add Authtorization in Dashboard Page

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,4 +1,19 @@
+import { useEffect } from "react"
+import { useNavigate } from "react-router-dom"
+
 export default function Dashboard(): JSX.Element {
+    const history = useNavigate();
+    const navigateToLogin = () => history('/');
+
+    const checkUser = () => {
+        const token = localStorage.getItem('token')
+        if (!token) { navigateToLogin() }
+    }
+
+    // const handleLogout = () => localStorage.removeItem('token')
+
+    useEffect(() => checkUser(), [])
+
     return <>
         <h1>ttle</h1>
         <h4>desc</h4>
