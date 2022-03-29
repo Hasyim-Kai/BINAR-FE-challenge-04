783ca61ddd751dd21b56129cc205e4eed80fd345
ichihatsu1 <ichihatsu1@gmail.com>
2022-03-29 09:52:48 +0700

    Refactor : Edit Left Menu Link & Responsiveness

--- src/components/LeftMenu.tsx
+++ src/components/LeftMenu.tsx
@@ -1,14 +1,30 @@
 import { Link, useLocation } from "react-router-dom";
 
 export default function LeftMenu(): JSX.Element {
-    // get the active Route path
-    let isLoginPage = useLocation().pathname === '/' ? true : false
     const sidebarItem: string = `px-7 py-3 hover:bg-white`;
+    
+    // get the active Route path
+    const currentRoute = useLocation().pathname;
+    let isLoginPage = currentRoute === '/' ? true : false
 
-    // if the active Route path is Login, then disable Sidebar
-    return isLoginPage ? <nav></nav> : <nav className="flex flex-col w-64 text-lg overflow-hidden bg-orange-600">
+    const dashboardLeftMenu: JSX.Element = <nav className="flex flex-col lg:w-64 w-0 text-lg overflow-hidden bg-orange-600">
         <h6 className="p-6 text-xl"><b>DASHBOARD</b></h6>
         <hr />
         <Link to={"/dashboard"} className={sidebarItem}><b>Dashboard</b></Link>
     </nav>
+
+    const carsLeftMenu: JSX.Element = <nav className="flex flex-col lg:w-64 w-0 text-lg overflow-hidden bg-orange-600">
+        <h6 className="p-6 text-xl"><b>CARS</b></h6>
+        <hr />
+        <Link to={"/carlist"} className={sidebarItem}><b>List Car</b></Link>
+    </nav>
+
+    // if the active Route path is Login, then disable Sidebar
+    if (isLoginPage) {
+        return <nav></nav>;
+    } else if (currentRoute === '/dashboard') {
+        return dashboardLeftMenu;
+    } else {
+        return carsLeftMenu;
+    }
 }
