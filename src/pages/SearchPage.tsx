e4db7b4fb7e50903a9afb7896659ef19933b1b35
Kai-69 <ichihatsu1@gmail.com>
2022-04-06 06:09:01 +0700

    Refactor : Edit Search Page Style

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -5,12 +5,12 @@ export default function SearchPage() {
 
     return <>
         <div className={flexDisplay}>
-            <div className="ml-9 lg:ml-14 xl:ml-24 2xl:ml-48 my-16 lg:my-auto">
+            <div className="ml-9 lg:ml-14 xl:ml-24 2xl:ml-48 my-16 lg:my-auto flex-1">
                 <h2 className="text-4xl font-bold mb-4">Sewa & Rental Mobil Terbaik di kawasan (Lokasimu)</h2>
-                <p className="">Selamat datang di Binar Car Rental. Kami menyediakan mobil kualitas
+                <p className="max-w-md">Selamat datang di Binar Car Rental. Kami menyediakan mobil kualitas
                     terbaik dengan harga terjangkau. Selalu siap melayani kebutuhanmuuntuk sewa mobil selama 24 jam.</p>
             </div>
-            <img className="w-full" src="./images/search_page_car.png" alt="Car" />
+            <img className="flex-1" src="./images/search_page_car.png" alt="Car" />
         </div>
 
         {/* SEARCH SECTION */}
