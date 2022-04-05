155d34fca766753f3b7f663ac368e817cf1e5973
Kai-69 <ichihatsu1@gmail.com>
2022-04-06 06:06:48 +0700

    Refactor : Edit Car Card Style

--- src/components/CarCard.tsx
+++ src/components/CarCard.tsx
@@ -1,20 +1,18 @@
 import { Link } from "react-router-dom";
 
 export default function CarCard() {
-    return <div className="bg-white p-7 max-w-sm">
-        <img className="pt-5 px-2 mb-10 w-72" src="images/car.png" alt="Car" />
+    const id: number = 1;
+
+    return <div className="bg-white mb-5 p-5 max-w-xs border-2 border-backgroundGrey rounded">
+        <img className="pt-5 px-1 mb-10 w-72" src="/images/car.png" alt="Car" />
 
         <p className="mb-2">Nama / Tipe Mobil</p>
-        <p className="mb-3 text-lg"><b>Rp. 430.000 / Hari</b></p>
-        <p className="mb-3 flex gap-2"><img src="images/icons/fi_key.svg" alt="key" /> Start rent - Finish rent</p>
-        <p className="mb-3 flex gap-2"><img src="images/icons/fi_clock.svg" alt="clock" /> Updated at 4 Apr 2022 09.00</p>
+        <p className="mb-2 text-lg"><b>Rp. 430.000 / Hari</b></p>
+        <p className="mb-3 text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        <p className="mb-3 flex"><img className="mr-2" src="/images/icons/fi_users.svg" alt="user" />aaaaa</p>
+        <p className="mb-3 flex"><img className="mr-2" src="/images/icons/fi_settings.svg" alt="car_type" />aaaa</p>
+        <p className="mb-3 flex"><img className="mr-2" src="/images/icons/fi_calendar.svg" alt="manufactured_date" />aaaa</p>
 
-        <div className="flex gap-3 mt-5">
-            <button className="flex gap-2 border-2 border-red-500 text-red-500 rounded py-3 px-6 flex-1">
-                <img src="images/icons/fi_trash.svg" alt="Del" /><b>Delete</b></button>
-            <Link to='/editcar' className="flex items-center justify-center gap-2 bg-green-500 text-white rounded-sm py-3 px-6 flex-1">
-                <img src="images/icons/fi_edit.svg" alt="Del" /><b>Edit</b></Link>
-
-        </div>
+        <Link to={`/car-rent-detail/${id}`} className="mt-5 flex justify-center text-xs bg-green-500 text-white rounded-sm py-4"><b>Pilih Mobil</b></Link>
     </div>
 }
